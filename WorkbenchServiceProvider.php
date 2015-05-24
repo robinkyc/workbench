94b10f04bd82ad072b8556f4495d5345a861c2a1
Gravitano <gravitano16@gmail.com>
2015-05-24 22:15:22 +0700

    PSR-2

--- WorkbenchServiceProvider.php
+++ WorkbenchServiceProvider.php
@@ -3,61 +3,59 @@
 use Illuminate\Support\ServiceProvider;
 use Pingpong\Workbench\Console\WorkbenchMakeCommand;
 
-class WorkbenchServiceProvider extends ServiceProvider {
+class WorkbenchServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Boot the package.
-	 * 
-	 * @return void
-	 */
-	public function boot()
-	{
-		$path = config_path('workbench.php');
+    /**
+     * Boot the package.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $path = config_path('workbench.php');
 
-		$this->publishes([
-			__DIR__ . '/config.php' => $path
-		], 'config');
+        $this->publishes([
+            __DIR__ . '/config.php' => $path
+        ], 'config');
 
-		if (file_exists($path)) {
-			$this->mergeConfigFrom($path, 'workbench');
-		}
-	}
+        if (file_exists($path)) {
+            $this->mergeConfigFrom($path, 'workbench');
+        }
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app->singleton('package.creator', function($app)
-		{
-			return new PackageCreator($app['files']);
-		});
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('package.creator', function ($app) {
+            return new PackageCreator($app['files']);
+        });
 
-		$this->app->singleton('command.workbench', function($app)
-		{
-			return new WorkbenchMakeCommand($app['package.creator']);
-		});
+        $this->app->singleton('command.workbench', function ($app) {
+            return new WorkbenchMakeCommand($app['package.creator']);
+        });
 
-		$this->commands('command.workbench');
-	}
+        $this->commands('command.workbench');
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('package.creator', 'command.workbench');
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('package.creator', 'command.workbench');
+    }
 }
